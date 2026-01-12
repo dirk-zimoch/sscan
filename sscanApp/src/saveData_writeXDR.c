91857816ebf1bd886945b21cd2da50844fb26669
Dirk Zimoch <dirk.zimoch@psi.ch>
2026-01-12 14:43:09 +0100

    for compatibility with ANSI-C have all variables before instructions

--- sscanApp/src/saveData_writeXDR.c
+++ sscanApp/src/saveData_writeXDR.c
@@ -717,12 +717,11 @@ LOCAL int fileStatus(char* fname)
 {
     struct stat status;
     int retVal;
-
-    errno = 0;
-
     size_t len;
     char lastChar;
     char *fnameLocal;
+
+    errno = 0;
 
     len = strlen(fname);
 
